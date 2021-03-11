acdad3301d45e6a1853f0a51d5ed338086f6bd61
Tiago Mota Barbosa <tiagomb.privado@gmail.com>
2021-03-11 03:30:14 -0300

    Adicionado ddlProduto com evento e Page_Load

---
+++ WebScript/Teste/EventoDropDownList.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Teste_EventoDropDownList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        trocaPanel(ddlProduto.SelectedValue);
+    }
+
+    protected void ddlProduto_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        trocaPanel(ddlProduto.SelectedValue);
+    }
+
+    protected void trocaPanel(string value)
+    {
+        switch (value)
+        {
+            case "0":
+                pnlProdutoF.Visible = false;
+                pnlProdutoM.Visible = true;
+                break;
+            case "1":
+                pnlProdutoF.Visible = true;
+                pnlProdutoM.Visible = false;
+                break;
+        }
+    }
+}
